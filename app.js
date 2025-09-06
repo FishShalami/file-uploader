7273f53c8348d528eb6181580b506f644ffca104
FishShalami <fishshalami@gmail.com>
2025-09-06 00:21:21 -0700

    Basic user registration working using bcrypt, stored on pg

--- app.js
+++ app.js
@@ -5,20 +5,46 @@ const session = require("express-session");
 require("dotenv").config();
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+const { pool } = require("./db/pool");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
+
+// --- GET ROUTES --- //
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up-form");
+});
+
+//--- POST ROUTES ---//
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(hashedPassword);
+    await pool.query(
+      `INSERT INTO "User" (username, password) VALUES ($1, $2)`,
+      [req.body.username, hashedPassword]
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 // require("./db/passport");
 // app.use(passport.initialize());
 // app.use(passport.session());
-
-// app.use(express.urlencoded({ extended: false }));
 
 // app.use(
 //   expressSession({
